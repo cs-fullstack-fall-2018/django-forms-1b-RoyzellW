6eb231ab0bd1a5315ebe346dea57ed5b5466876e
RoyzellW <royzellwalton@gmail.com>
2018-10-05 14:56:36 -0500

    Done

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    fWebsite = forms.CharField()
     text = forms.CharField(widget=forms.Textarea)
